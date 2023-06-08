d8c07354c8b7dada91163290553107914bf6a315
milkyfuckinway <0-0_0@bk.ru>
2023-06-09 01:48:42 +0300

    fix

--- src/components/views/LinksView.vue
+++ src/components/views/LinksView.vue
@@ -7,7 +7,7 @@ import TemplateSection from '@blocks/TemplateSection.vue';
 	<TemplateIntro
 		title="Контакты"
 		paragraph="И прочее" />
-		<TemplateSection title="CV"><a href="https://drive.google.com/file/d/1w5AmX_k4Hi-O6Bs1cS-PFDtRODud3oj8/view?usp=sharing">PDF</a></TemplateSection>
+		<TemplateSection title="CV"><a href="https://drive.google.com/file/d/1pHyJADef0bRRsDbYIJd2WzlXM7Ob8RGQ/view?usp=drive_link">PDF</a></TemplateSection>
 		<TemplateSection title="E-mail"><a href="mailto:0-0_0@bk.ru">0-0_0@bk.ru</a></TemplateSection>
 		<TemplateSection title="Github"><a href="https://github.com/milkyfuckinway">github.com/milkyfuckinway</a></TemplateSection>
 		<TemplateSection title="Telegram"><a href="https://t.me/milkyfuckinway">t.me/milkyfuckinway</a></TemplateSection>
